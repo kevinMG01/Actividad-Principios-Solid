b13d99d0f669f118a19a5bc07f77221360e9f1d8
kevinMG01 <montanokevin46@gmail.com>
2024-10-19 13:25:53 -0300

    ataque
    se creo el script para la lógica de ataque.

---
+++ Actividad Principios Solid/Assets/scripts/NPCAtaca.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCAtaca : MonoBehaviour
+{
+    public void atacar()
+    {
+        Debug.Log(" El NPC esta atacando");
+    }
+}
